f1dde0e045a6d295f8db604515db01ad9699814e
Aaron Miller <millrfz@amazon.com>
2021-10-19 12:09:51 -0700

    add check subnets utility

--- pkg/aws/ec2.go
+++ pkg/aws/ec2.go
@@ -20,6 +20,35 @@ func EC2CreateTags(resources []string, tags map[string]string) error {
 	return nil
 }
 
+func EC2DescribeTags(resources, tagsFilter []string) (*ec2.DescribeTagsOutput, error) {
+	sess := GetSession()
+	svc := ec2.New(sess)
+
+	filters := []*ec2.Filter{
+		{
+			Name:   aws.String("resource-id"),
+			Values: aws.StringSlice(resources),
+		},
+	}
+
+	if len(tagsFilter) > 0 {
+		filters = append(filters, &ec2.Filter{
+			Name:   aws.String("key"),
+			Values: aws.StringSlice(tagsFilter),
+		})
+	}
+
+	result, err := svc.DescribeTags(&ec2.DescribeTagsInput{
+		Filters: filters,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
 func createEC2Tags(tags map[string]string) (ec2tags []*ec2.Tag) {
 	for k, v := range tags {
 		ec2tags = append(ec2tags, &ec2.Tag{
--- pkg/util/tag_subnets.go
+++ pkg/util/tag_subnets.go
@@ -8,7 +8,10 @@ import (
 	"github.com/aws/aws-sdk-go/aws/awserr"
 )
 
-func TagSubnets(clusterName string) error {
+// kubernetes.io/cluster/<clusterName>
+const K8stag = `kubernetes.io/cluster/%s`
+
+func getSubnets(clusterName string) (string, error) {
 	stackName := "eksctl-" + clusterName + "-cluster"
 
 	stack, err := aws.CloudFormationDescribeStack(stackName)
@@ -16,12 +19,12 @@ func TagSubnets(clusterName string) error {
 		if awsErr, ok := err.(awserr.Error); ok {
 			switch awsErr.Code() {
 			case "ValidationError":
-				return fmt.Errorf("cloudformation stack %q not found, is this an eksctl cluster?", stackName)
+				return "", fmt.Errorf("cloudformation stack %q not found, is this an eksctl cluster?", stackName)
 			default:
-				return err
+				return "", err
 			}
 		}
-		return err
+		return "", err
 	}
 
 	subnets := ""
@@ -32,11 +35,42 @@ func TagSubnets(clusterName string) error {
 	}
 
 	if subnets == "" {
-		return fmt.Errorf("no private subnets found in cloudformation stack %q", stackName)
+		return "", fmt.Errorf("no private subnets found in cloudformation stack %q", stackName)
+	}
+
+	return subnets, nil
+}
+
+func CheckSubnets(clusterName string) error {
+	subnetsString, err := getSubnets(clusterName)
+	if err != nil {
+		return err
+	}
+
+	subnets := strings.Split(subnetsString, ",")
+	tag := fmt.Sprintf(K8stag, clusterName)
+	tagsFilter := []string{tag}
+
+	result, err := aws.EC2DescribeTags(subnets, tagsFilter)
+	if err != nil {
+		return err
+	}
+
+	if len(result.Tags) == 0 {
+		return fmt.Errorf("required tag %q not found on any of the following private subnets:\n%s", tag, strings.Join(subnets, "\n"))
+	}
+
+	return nil
+}
+
+func TagSubnets(clusterName string) error {
+	subnets, err := getSubnets(clusterName)
+	if err != nil {
+		return err
 	}
 
 	tags := map[string]string{
-		"kubernetes.io/cluster/" + clusterName: "",
+		fmt.Sprintf(K8stag, clusterName): "",
 	}
 
 	fmt.Println("Tagging subnets: " + subnets)
