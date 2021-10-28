50840d1f984b08824e78fa2ffb6a200ff03801ca
Aaron Miller <millrfz@amazon.com>
2021-10-28 10:00:16 -0700

    update version command to use build flags

--- cmd/version.go
+++ cmd/version.go
@@ -6,11 +6,29 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	version string
+	commit  string
+	date    string
+)
+
+type Version struct {
+	Version string
+	Date    string
+	Commit  string
+}
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "version of eksdemo",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Version 0.1.0-alpha-2021-10-20")
+		eksdemoVersion := Version{
+			Version: version,
+			Date:    date,
+			Commit:  commit,
+		}
+
+		fmt.Printf("eksdemo version info: %#v\n", eksdemoVersion)
 	},
 }
 
