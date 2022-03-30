9febd2c646833a2ac2bff6b9b40384506cc52487
Aaron Miller <millrfz@amazon.com>
2022-03-29 18:07:52 -0700

    update: remove subnet check for karpenter, it's not needed any more

--- pkg/application/karpenter/options.go
+++ pkg/application/karpenter/options.go
@@ -3,15 +3,10 @@ package karpenter
 import (
 	"eksdemo/pkg/application"
 	"eksdemo/pkg/cmd"
-	"eksdemo/pkg/util"
-	"errors"
-	"fmt"
 )
 
 type KarpenterOptions struct {
 	application.ApplicationOptions
-
-	skipSubnetCheck bool
 }
 
 func NewOptions() (options *KarpenterOptions, flags cmd.Flags) {
@@ -26,30 +21,6 @@ func NewOptions() (options *KarpenterOptions, flags cmd.Flags) {
 		},
 	}
 
-	flags = cmd.Flags{
-		&cmd.BoolFlag{
-			CommandFlag: cmd.CommandFlag{
-				Name:        "skip-subnet-check",
-				Description: "don't check subnets for required Karpenter tags",
-			},
-			Option: &options.skipSubnetCheck,
-		},
-	}
+	flags = cmd.Flags{}
 	return
-}
-
-func (o *KarpenterOptions) PreDependencies(action application.Action) error {
-	if o.skipSubnetCheck || action == application.Uninstall {
-		return nil
-	}
-
-	if err := util.CheckSubnets(o.ClusterName); err != nil {
-		errMsg := err.Error()
-		errMsg += fmt.Sprintf("\n\nKarpenter requires subnets tagged with %q to perform subnet discovery\n",
-			fmt.Sprintf(util.K8stag, o.ClusterName))
-		errMsg += fmt.Sprintf("Either run `eksdemo util tag-subnets -c %s` or use the `--skip-subnet-check` flag", o.ClusterName)
-		return errors.New(errMsg)
-	}
-
-	return nil
 }
