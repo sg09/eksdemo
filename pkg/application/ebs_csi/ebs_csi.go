a89d133a08b2f9815af0900e3f0eed1e5cc4b8a0
Aaron Miller <millrfz@amazon.com>
2022-04-19 22:17:23 -0400

    update: ebs-csi driver to use new managed IAM policy

--- pkg/application/ebs_csi/ebs_csi.go
+++ pkg/application/ebs_csi/ebs_csi.go
@@ -28,10 +28,8 @@ func NewApp() *application.Application {
 				CommonOptions: resource.CommonOptions{
 					Name: "ebs-csi-irsa",
 				},
-				PolicyType: irsa.PolicyDocument,
-				PolicyDocTemplate: &template.TextTemplate{
-					Template: policyDocument,
-				},
+				PolicyType: irsa.PolicyARNs,
+				Policy:     []string{"arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"},
 			}),
 		},
 
@@ -69,95 +67,4 @@ controller:
     name: {{ .ServiceAccount }}
 image:
   tag: {{ .Version }}
-`
-
-const policyDocument = `
-Version: '2012-10-17'
-Statement:
-- Effect: Allow
-  Action:
-  - ec2:CreateSnapshot
-  - ec2:AttachVolume
-  - ec2:DetachVolume
-  - ec2:ModifyVolume
-  - ec2:DescribeAvailabilityZones
-  - ec2:DescribeInstances
-  - ec2:DescribeSnapshots
-  - ec2:DescribeTags
-  - ec2:DescribeVolumes
-  - ec2:DescribeVolumesModifications
-  Resource: "*"
-- Effect: Allow
-  Action:
-  - ec2:CreateTags
-  Resource:
-  - arn:aws:ec2:*:*:volume/*
-  - arn:aws:ec2:*:*:snapshot/*
-  Condition:
-    StringEquals:
-      ec2:CreateAction:
-      - CreateVolume
-      - CreateSnapshot
-- Effect: Allow
-  Action:
-  - ec2:DeleteTags
-  Resource:
-  - arn:aws:ec2:*:*:volume/*
-  - arn:aws:ec2:*:*:snapshot/*
-- Effect: Allow
-  Action:
-  - ec2:CreateVolume
-  Resource: "*"
-  Condition:
-    StringLike:
-      aws:RequestTag/ebs.csi.aws.com/cluster: 'true'
-- Effect: Allow
-  Action:
-  - ec2:CreateVolume
-  Resource: "*"
-  Condition:
-    StringLike:
-      aws:RequestTag/CSIVolumeName: "*"
-- Effect: Allow
-  Action:
-  - ec2:CreateVolume
-  Resource: "*"
-  Condition:
-    StringLike:
-      aws:RequestTag/kubernetes.io/cluster/*: owned
-- Effect: Allow
-  Action:
-  - ec2:DeleteVolume
-  Resource: "*"
-  Condition:
-    StringLike:
-      ec2:ResourceTag/ebs.csi.aws.com/cluster: 'true'
-- Effect: Allow
-  Action:
-  - ec2:DeleteVolume
-  Resource: "*"
-  Condition:
-    StringLike:
-      ec2:ResourceTag/CSIVolumeName: "*"
-- Effect: Allow
-  Action:
-  - ec2:DeleteVolume
-  Resource: "*"
-  Condition:
-    StringLike:
-      ec2:ResourceTag/kubernetes.io/cluster/*: owned
-- Effect: Allow
-  Action:
-  - ec2:DeleteSnapshot
-  Resource: "*"
-  Condition:
-    StringLike:
-      ec2:ResourceTag/CSIVolumeSnapshotName: "*"
-- Effect: Allow
-  Action:
-  - ec2:DeleteSnapshot
-  Resource: "*"
-  Condition:
-    StringLike:
-      ec2:ResourceTag/ebs.csi.aws.com/cluster: 'true'
 `
