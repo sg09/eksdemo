0aa2e4ca123667a11582c6d738a36c79b4f89daa
Aaron Miller <millrfz@amazon.com>
2022-10-23 21:05:30 -0700

    update: `kube-prometheus` to Chart 41.5.1, PromOperator v0.60.1

--- pkg/application/kube_prometheus/options.go
+++ pkg/application/kube_prometheus/options.go
@@ -14,8 +14,8 @@ func newOptions() (options *KubePrometheusOptions, flags cmd.Flags) {
 	options = &KubePrometheusOptions{
 		ApplicationOptions: &application.ApplicationOptions{
 			DefaultVersion: &application.LatestPrevious{
-				LatestChart:   "39.6.0",
-				Latest:        "v0.58.0",
+				LatestChart:   "41.5.1",
+				Latest:        "v0.60.1",
 				PreviousChart: "34.10.0",
 				Previous:      "v0.55.0",
 			},
