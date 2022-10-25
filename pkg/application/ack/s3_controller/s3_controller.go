688076ec4cff83228cf6e50c622274c76bd03587
Aaron Miller <millrfz@amazon.com>
2022-10-24 17:19:48 -0700

    update: `ack-s3-controller` to v0.1.5

--- pkg/application/ack/s3_controller/s3_controller.go
+++ pkg/application/ack/s3_controller/s3_controller.go
@@ -14,7 +14,7 @@ import (
 // GitHub:  https://github.com/aws-controllers-k8s/s3-controller
 // Helm:    https://github.com/aws-controllers-k8s/s3-controller/tree/main/helm
 // Repo:    https://gallery.ecr.aws/aws-controllers-k8s/s3-controller
-// Version: Latest is v0.1.2 (as of 06/21/22)
+// Version: Latest is v0.1.5 (as of 10/24/22)
 
 func NewApp() *application.Application {
 	app := &application.Application{
@@ -40,10 +40,10 @@ func NewApp() *application.Application {
 			Namespace:      "ack-system",
 			ServiceAccount: "ack-s3-controller",
 			DefaultVersion: &application.LatestPrevious{
-				LatestChart:   "v0.1.2",
-				Latest:        "v0.1.2",
-				PreviousChart: "v0.1.1",
-				Previous:      "v0.1.1",
+				LatestChart:   "v0.1.5",
+				Latest:        "v0.1.5",
+				PreviousChart: "v0.1.2",
+				Previous:      "v0.1.2",
 			},
 		},
 
