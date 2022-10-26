131e7103cc712f72cbad88772e0565b980400adf
Aaron Miller <millrfz@amazon.com>
2022-10-26 16:42:09 -0700

    fix: add retry to kubernetes resource manager fixes karpenter post-install error creating provisioner if webhook isn't ready

--- pkg/kubernetes/client.go
+++ pkg/kubernetes/client.go
@@ -5,9 +5,13 @@ import (
 	"fmt"
 	"net/url"
 	"strings"
+	"time"
 
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/cli-runtime/pkg/resource"
+	"k8s.io/client-go/util/retry"
 	cmdutil "k8s.io/kubectl/pkg/cmd/util"
 )
 
@@ -15,7 +19,7 @@ func CreateResources(kubeContext, manifest string) error {
 	getter := genericclioptions.NewConfigFlags(true)
 	getter.Context = &kubeContext
 	factory := cmdutil.NewFactory(getter)
-	builder := factory.NewBuilder().Unstructured()
+	builder := factory.NewBuilder().Unstructured().ContinueOnError()
 
 	switch {
 	case strings.Index(manifest, "http://") == 0 || strings.Index(manifest, "https://") == 0:
@@ -35,18 +39,28 @@ func CreateResources(kubeContext, manifest string) error {
 		return err
 	}
 
+	// Initial use case for Retry is Karpenter. Helm wait doesn't wait for Webhooks
+	// https://github.com/cert-manager/cert-manager/issues/2908
+	tenSecondRetry := wait.Backoff{
+		Steps:    5,
+		Duration: 1 * time.Second,
+	}
+
 	for _, info := range infos {
-		fmt.Printf("Creating %s: %s", info.Object.GetObjectKind().GroupVersionKind().Kind, info.Name)
+		fmt.Printf("Creating %s %q", info.Object.GetObjectKind().GroupVersionKind().Kind, info.Name)
 		if info.Namespace != "" {
-			fmt.Printf(" in namespace: %s", info.Namespace)
+			fmt.Printf(" in namespace %q", info.Namespace)
 		}
 		fmt.Println()
 
-		obj, err := resource.NewHelper(info.Client, info.Mapping).Create(info.Namespace, true, info.Object)
+		err = retry.OnError(tenSecondRetry, errors.IsInternalError, func() (err error) {
+			_, err = resource.NewHelper(info.Client, info.Mapping).Create(info.Namespace, true, info.Object)
+			return
+		})
+
 		if err != nil {
 			fmt.Printf("Warning: failed to create resource: %s\n", err)
 		}
-		_ = obj
 	}
 
 	return nil
