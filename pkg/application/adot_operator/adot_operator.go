ab9dd9c376d39110393e8252a44fee861378b644
Aaron Miller <millrfz@amazon.com>
2023-01-24 21:53:33 -0700

    update: `adot-operator` to v0.66.0

--- pkg/application/adot_operator/adot_operator.go
+++ pkg/application/adot_operator/adot_operator.go
@@ -10,25 +10,26 @@ import (
 // Docs:    https://opentelemetry.io/docs/
 // Docs:    https://github.com/open-telemetry/opentelemetry-operator/blob/main/docs/api.md
 // GitHub:  https://github.com/open-telemetry/opentelemetry-operator
-// Helm:    https://github.com/open-telemetry/opentelemetry-helm-charts
-// Repo:    ghcr.io/open-telemetry/opentelemetry-operator/opentelemetry-operator
-// Version: Latest is v0.53.0 (as of 06/22/22)
+// Helm:    https://github.com/open-telemetry/opentelemetry-helm-charts/tree/main/charts/opentelemetry-operator
+// Repo:    https://gallery.ecr.aws/aws-observability/adot-operator
+// Repo:    https://gallery.ecr.aws/aws-observability/mirror-kube-rbac-proxy
+// Version: Latest is v0.66.0, Chart 0.21.4 (as of 01/24/23)
 
 func NewApp() *application.Application {
 	app := &application.Application{
 		Command: cmd.Command{
 			Name:        "adot-operator",
-			Description: "AWS Distro for OpenTelemetry Operator",
-			Aliases:     []string{"adot", "otel-operator", "otel"},
+			Description: "AWS Distro for OpenTelemetry (ADOT) Operator",
+			Aliases:     []string{"adot"},
 		},
 
 		Options: &application.ApplicationOptions{
 			Namespace: "adot-system",
 			DefaultVersion: &application.LatestPrevious{
-				LatestChart:   "0.8.2",
-				Latest:        "v0.53.0",
-				PreviousChart: "0.7.0",
-				Previous:      "v0.51.0",
+				LatestChart:   "0.21.4",
+				Latest:        "v0.66.0",
+				PreviousChart: "0.17.2",
+				Previous:      "v0.63.1",
 			},
 			// Service Account name isn't flexible
 			// https://github.com/open-telemetry/opentelemetry-helm-charts/blob/main/charts/opentelemetry-operator/templates/serviceaccount.yaml
@@ -52,5 +53,29 @@ replicaCount: 1
 nameOverride: adot-operator
 manager:
   image:
+    repository: public.ecr.aws/aws-observability/adot-operator
     tag: {{ .Version }}
+  collectorImage:
+    repository: public.ecr.aws/aws-observability/aws-otel-collector
+    tag: v0.24.1
+  targetAllocatorImage:
+    repository: public.ecr.aws/aws-observability/mirror-target-allocator
+    tag: 0.66.0
+  autoInstrumentationImage:
+    java:
+      repository: public.ecr.aws/aws-observability/mirror-autoinstrumentation-java
+      tag: 1.20.2
+    nodejs:
+      repository: public.ecr.aws/aws-observability/mirror-autoinstrumentation-nodejs
+      tag: 0.31.0
+    python:
+      repository: public.ecr.aws/aws-observability/mirror-autoinstrumentation-python
+      tag: 0.35b0
+    dotnet:
+      repository: public.ecr.aws/aws-observability/mirror-autoinstrumentation-dotnet
+      tag: 0.5.0
+kubeRBACProxy:
+  image:
+    repository: public.ecr.aws/aws-observability/mirror-kube-rbac-proxy
+    tag: v0.11.0
 `
